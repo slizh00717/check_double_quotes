6aa9a4cc34a52f53cce9c0066023cda0e90fb599
Anatoly Slizhevsky <anatoly.slizhevsky@betera.by>
2024-06-25 14:37:53 +0300

    change pre-commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='check_double_quotes',  # Название вашего хука
+    name='check-double-quotes',  # Название вашего хука
     version='1.0',
     py_modules=['check_double_quotes'],  # Укажите здесь имя вашего модуля
     entry_points={
